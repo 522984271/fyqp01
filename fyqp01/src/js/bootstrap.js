ffd151f1325cb6d3b53768ef257c3ebef1e5f475
Xuan <522984271@qq.com>
2020-05-15 00:47:59 +0800

    '初始化项目'

---
+++ fyqp01/src/js/bootstrap.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'zico/css/zico.min.css';
+import Button from 'bootstrap/js/button';
+
+Vue.use(Button);
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin)
---
+++ fyqp01/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './js/elementUI';
+import './js/antd';
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
